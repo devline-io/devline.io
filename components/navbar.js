19a41f1355961b1c800bd8c5d08e26b14be596cc
GreenLightning07 <cwnaze@gmail.com>
2023-02-15 20:44:10 -0500

    v0.1.1
    created navbar looks and added some sample text to home page

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../styles/navbar.module.css'
 
 const navItems = ['Catalog', 'About', 'Pricing'];
 
 export default function Navbar() {
     return (
-        <div>
+        <div className={styles.container}>
             <Image src='/logo.svg' width={200} height={35}/>
-            <ul>
-                {navItems.map((item) => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
-            <button>Sign Up</button>
+            <div className={styles.navLinks}>
+                <ul className={styles.navLinks}>
+                    {navItems.map((item) => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ul>
+                <button className={styles.signUp}>Sign Up</button>
+            </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
+import styles from '../styles/homepage.module.css'
 
 export default function HomePage() {
   return (
-    <Navbar/>
+    <div>
+      <Head>
+        <title>Devline.io</title>
+      </Head>
+      <Navbar/>
+      <div>
+        <h1 className={styles.tagline}>Learn to Code <span>SMARTER,</span><br/> Not <span>HARDER</span></h1>
+      </div>
+    </div>
   );
 }
