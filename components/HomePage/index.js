710c537a29d967435d0cfde50c87ea7f756717dd
GreenLightning07 <cwnaze@gmail.com>
2023-02-23 12:58:29 -0500

    created pricing section

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -46,61 +46,101 @@ export default function HomePage() {
             </Head>
             <Navbar/>
             <main>
-            <div className={styles.hero}>
-                <motion.div className={styles.titleContainer} initial="hidden" whileInView="show" variants={container}>
-                <motion.h1 variants={fadeIn} className={styles.title}>Code <span>SMARTER,</span><br/> Not <span>HARDER</span></motion.h1>
-                <motion.p variants={fadeIn} className={styles.subtitle}>Learn to code the <span>RIGHT WAY</span> with engaging, interactive tutorials</motion.p>
-                </motion.div>
-                <motion.form initial="hidden" whileInView="show" delay={0.5} variants={container}
-                className={styles.heroForm}>
-                <motion.div variants={fadeIn}>
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' id='email'/>
-                </motion.div>
-                <motion.div variants={fadeIn}>
-                    <label htmlFor='password'>Password</label>
-                    <input type='password' id='password'/>
-                </motion.div>
-                <motion.div variants={fadeIn}>
-                    <label htmlFor='confirm-password'>Confirm Your Password</label>
-                    <input type='password' id='confirm-password'/>
-                </motion.div>
-                <motion.button variants={fadeIn}>Sign Up</motion.button>
-                </motion.form>
-            </div>
-
-            <Darkdivider dividerPosition='top'/>
-
-            <motion.div className={styles.catalog} initial="hidden" whileInView="show" variants={container}>
-                <motion.h2 variants={fadeIn}>./Catalog</motion.h2>
-                <motion.div className={styles.carousel} initial="hidden" whileInView="show" variants={container}>
-                    <CatalogImages/>
-                    <CatalogImages/>       
-                </motion.div>
-                <motion.button whileHover={{scale: 1.1,}} transition={{duration: 0.1}} variants={fadeIn} className={styles.lightButton}>See Full Catalog</motion.button>
-            </motion.div>
-
-            <Darkdivider dividerPosition='bottom'/>
-
-            <motion.div className={styles.about} initial="hidden" whileInView="show" variants={container}>
-                <motion.h2 variants={fadeIn}>./About</motion.h2>
-                <div className={styles.aboutContent}>
-                    <div className={styles.mission}>
-                    <motion.h3 variants={fadeIn}>Our Mission</motion.h3>
-                    <motion.p variants={fadeIn}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit. Ut eu sem integer vitae. At tempor commodo ullamcorper a lacus vestibulum. Tincidunt praesent semper feugiat nibh sed.</motion.p>
-                    </div>
-                    <div className={styles.verticalRule}/>
-                    <div className={styles.structure}>
-                    <motion.h3 variants={fadeIn}>Course Structure</motion.h3>
-                    <motion.p variants={fadeIn}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit. Ut eu sem integer vitae. At tempor commodo ullamcorper a lacus vestibulum. Tincidunt praesent semper feugiat nibh sed.</motion.p>
-                    </div>
+                <div className={styles.hero}>
+                    <motion.div className={styles.titleContainer} initial="hidden" whileInView="show" variants={container}>
+                    <motion.h1 variants={fadeIn} className={styles.title}>Code <span>SMARTER,</span><br/> Not <span>HARDER</span></motion.h1>
+                    <motion.p variants={fadeIn} className={styles.subtitle}>Learn to code the <span>RIGHT WAY</span> with engaging, interactive tutorials</motion.p>
+                    </motion.div>
+                    <motion.form initial="hidden" whileInView="show" delay={0.5} variants={container}
+                    className={styles.heroForm}>
+                    <motion.div variants={fadeIn}>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' id='email'/>
+                    </motion.div>
+                    <motion.div variants={fadeIn}>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' id='password'/>
+                    </motion.div>
+                    <motion.div variants={fadeIn}>
+                        <label htmlFor='confirm-password'>Confirm Your Password</label>
+                        <input type='password' id='confirm-password'/>
+                    </motion.div>
+                    <motion.button variants={fadeIn}>Sign Up</motion.button>
+                    </motion.form>
                 </div>
-            </motion.div>
 
-            <Darkdivider dividerPosition='top'/>
-            <div className={styles.pricing}>
-                <h2>./Pricing</h2>
-            </div>
+                <Darkdivider dividerPosition='top'/>
+
+                <motion.div className={styles.catalog} initial="hidden" whileInView="show" variants={container}>
+                    <motion.h2 variants={fadeIn}>./Catalog</motion.h2>
+                    <motion.div className={styles.carousel} initial="hidden" whileInView="show" variants={container}>
+                        <CatalogImages/>
+                        <CatalogImages/>       
+                    </motion.div>
+                    <motion.button whileHover={{scale: 1.1,}} transition={{duration: 0.1}} variants={fadeIn} className={styles.lightButton}>See Full Catalog</motion.button>
+                </motion.div>
+
+                <Darkdivider dividerPosition='bottom'/>
+
+                <motion.div className={styles.about} initial="hidden" whileInView="show" variants={container}>
+                    <motion.h2 variants={fadeIn}>./About</motion.h2>
+                    <div className={styles.aboutContent}>
+                        <div className={styles.mission}>
+                        <motion.h3 variants={fadeIn}>Our Mission</motion.h3>
+                        <motion.p variants={fadeIn}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit. Ut eu sem integer vitae. At tempor commodo ullamcorper a lacus vestibulum. Tincidunt praesent semper feugiat nibh sed.</motion.p>
+                        </div>
+                        <div className={styles.verticalRule}/>
+                        <div className={styles.structure}>
+                        <motion.h3 variants={fadeIn}>Course Structure</motion.h3>
+                        <motion.p variants={fadeIn}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit. Ut eu sem integer vitae. At tempor commodo ullamcorper a lacus vestibulum. Tincidunt praesent semper feugiat nibh sed.</motion.p>
+                        </div>
+                    </div>
+                </motion.div>
+
+                <Darkdivider dividerPosition='top'/>
+                <div className={styles.pricing}>
+                    <h2>./Pricing</h2>
+                    <section className={styles.pricingContainer}>
+                        <div>
+                            <h3>Free</h3>
+                            <hr/>
+                            <p>Free</p>
+                            <hr/>
+                            <ul>
+                                <li>Limited Access to Courses</li>
+                                <li className={styles.strikethrough}>Interactive Tutorials</li>
+                                <li className={styles.strikethrough}>Written and Video Tutorials</li>
+                                <li className={styles.strikethrough}>Project Based Learning</li>
+                            </ul>
+                        </div>
+                        <div className={styles.featuredContainer}>
+                            <div className={styles.featured}>
+                                <h3>Yearly</h3>
+                                <hr/>
+                                <p>$99.99/year</p>
+                                <hr/>
+                                <ul>
+                                    <li>Full Access to All Courses</li>
+                                    <li>Interactive Tutorials</li>
+                                    <li>Written and Video Tutorials</li>
+                                    <li>Project Based Learning</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <h3>Monthly</h3>
+                            <hr/>
+                            <p>$9.99/month</p>
+                            <hr/>
+                            <ul>
+                                <li>Full Access to All Courses</li>
+                                <li>Interactive Tutorials</li>
+                                <li>Written and Video Tutorials</li>
+                                <li>Project Based Learning</li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
             </main>
         </div>
     );
