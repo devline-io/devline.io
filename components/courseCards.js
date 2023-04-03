ba1f2d33702d58d4383229dd65ede7a3ab7b4eca
GreenLightning07 <cwnaze@gmail.com>
2023-04-03 16:37:57 -0400

    Revert "Merge branch 'profile' of https://github.com/devline-io/devline.io into profile"
    This reverts commit 73be8653482c2699ee90da51362fd9f8287b2b16, reversing
    changes made to 6252b4ef4add47c7289006929b9e3920ac783a41.

--- components/courseCards.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/courseCards.module.css';
-
-export default function courseCards({ courseName }) {
-    return(
-        <div className={styles.card}>
-            <h3 className={styles.title}>{courseName}</h3>
-        </div>
-    );
-}
