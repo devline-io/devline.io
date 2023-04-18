f4fb1cd5fb02af30e131b8addb1122373217e249
devline-io <124753414+devline-io@users.noreply.github.com>
2023-04-18 07:45:39 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,39 @@
+import { getAuth, signOut } from 'firebase/auth';
 import Image from 'next/image';
+import { useRef, useState } from 'react';
 import styles from '../styles/navbar.module.css';
+import { initFirebase } from './firebase';
+import Link from 'next/link';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
 
 export default function Navbar(props) {
+    initFirebase();
+    const auth = getAuth();
 
+    const [user] = useAuthState(auth);
+
+    const router = useRouter();
+
+    const profileMenu = useRef(null);
+    const [menuToggle, setMenuToggle] = useState(true);
+
+    const openProfileMenu = (e) => {
+        e.preventDefault();
+        if (menuToggle){
+            profileMenu.current.style.display = 'block';
+            setMenuToggle(false);
+        }
+        else {
+            profileMenu.current.style.display = 'none';
+            setMenuToggle(true);
+        }
+    }    
+
+    const handleSignOut = () => {
+        signOut(auth);
+        router.push('/');
+    }
     
     return (
         <div ref={props.navbarRef} className={styles.wrapper}>
@@ -15,8 +46,13 @@ export default function Navbar(props) {
                         ))}
                     </ul>
                     {props.button}
-                    {props.username}
-                    {props.signout}
+                    {props.profilePic && <div className={styles.profileContainer}>
+                        <Image onClick={openProfileMenu} src={props.profilePic} width={48} height={48}/>
+                        <div ref={profileMenu} className={styles.profileMenu}>
+                            <p>{user.displayName}</p>
+                            <Link onClick={handleSignOut} href='/'>Sign Out</Link>
+                        </div>
+                    </div>}
                 </div>
             </div>
         </div>
