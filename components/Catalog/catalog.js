d3038470cbc95d65e10a2305d6a6313d819291be
GreenLightning07 <cwnaze@gmail.com>
2023-04-23 17:07:34 -0400

    fix: errors from refactoring catalog system

--- components/Catalog/catalog.js
+++ components/Catalog/catalog.js
@@ -11,31 +11,34 @@ import { container, fadeIn } from '../HomePage/homepage';
 export default function Catalog({courses}) {
     const router = useRouter();
 
-    // const imageRefs = [
-    //     useRef(null),
-    //     useRef(null),
-    //     useRef(null),
-    //     useRef(null),
-    //     useRef(null)
-    // ];
+    const imageRefs = [
+        useRef(null),
+        useRef(null),
+        useRef(null),
+        useRef(null),
+        useRef(null),
+        useRef(null)
+    ];
 
-    // useEffect(() => {
-    //     for (let index = 0; index < imageRefs.length; index++) {
-    //         let ref = imageRefs[index].current;
-    //         ref.addEventListener('mousemove', (event) => {
-    //             const width   = ref.offsetWidth;
-    //             const height  = ref.offsetHeight;
-    //             const centerX = ref.offsetLeft + width/2;
-    //             const centerY = ref.offsetTop + height/2;
-    //             const mouseX  = event.pageX - centerX;
-    //             const mouseY  = event.pageY - centerY;
-    //             const rotateX = -10 * mouseY / (height / 2);
-    //             const rotateY = 10 * mouseX / (width / 2)
+    useEffect(() => {
+        for (let index = 0; index < imageRefs.length; index++) {
+            if(imageRefs[index].current != null) {
+                let ref = imageRefs[index].current;
+                ref.addEventListener('mousemove', (event) => {
+                    const width   = ref.offsetWidth;
+                    const height  = ref.offsetHeight;
+                    const centerX = ref.offsetLeft + width/2;
+                    const centerY = ref.offsetTop + height/2;
+                    const mouseX  = event.pageX - centerX;
+                    const mouseY  = event.pageY - centerY;
+                    const rotateX = -10 * mouseY / (height / 2);
+                    const rotateY = 10 * mouseX / (width / 2)
 
-    //             ref.style.transform = `perspective(1000px) scale(1.05) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    //     });
-    //     }
-    // });
+                    ref.style.transform = `perspective(1000px) scale(1.05) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+                });
+            }
+        }
+    });
 
     const navItems = [
         <Link href='/'>Home</Link>,
@@ -64,7 +67,7 @@ export default function Catalog({courses}) {
                         containerClass={styles.container}
                         hoverAnimation={{scale: 1.01, filter: "drop-shadow(0 0 0.75rem #18122B)"}}
                         hasDescription={true}
-                        // imageRefs={imageRefs}
+                        imageRefs={imageRefs}
                         fadeIn={fadeIn}
                         courses={courses}
                     />
--- components/catalogimages.js
+++ components/catalogimages.js
@@ -3,13 +3,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function CatalogImages({ courses, fadeIn, containerClass, imageContainer, hasDescription, hoverAnimation, imageRefs }) {    
-    const thumbnails = [
-        '/thumbnails/placeholderThumbnail1.png',
-        '/thumbnails/placeholderThumbnail2.png',
-        '/thumbnails/placeholderThumbnail3.png',
-        '/thumbnails/placeholderThumbnail4.png',
-        '/thumbnails/placeholderThumbnail5.png'
-    ];
 
     const router = useRouter();
     const thumbnailPage = [
@@ -22,9 +15,9 @@ export default function CatalogImages({ courses, fadeIn, containerClass, imageCo
 
     if(hasDescription == false) {
         return (
-            thumbnails.map(thumbnail => (
+            courses.map((course, index) => (
             <motion.div className={containerClass} variants={fadeIn} key={thumbnail}>
-                <Image alt={thumbnail} src={thumbnail} fill sizes='25vw' />
+                <Image alt={course.thumbnail + index} src={course.thumbnail} fill sizes='25vw' />
             </motion.div>
         ))
         );
@@ -36,12 +29,12 @@ export default function CatalogImages({ courses, fadeIn, containerClass, imageCo
     
     return (
         courses.map((course, index) => (
-            <motion.div key={course}
+            <motion.div key={course + index}
              variants={fadeIn} 
              initial={{opacity: 0, y: 15, filter: 'none'}} 
              whileInView="show" 
              onClick={thumbnailPage[index]} 
-             // ref={imageRefs[index]} 
+             ref={imageRefs[index]} 
              className={containerClass} 
              whileHover={hoverAnimation}
              transition={{duration: 0.1, ease: "linear"}}>
