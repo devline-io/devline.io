9589649b0c92801227d7028cc347dc79e4ff4e7a
GreenLightning07 <cwnaze@gmail.com>
2023-08-03 03:06:37 -0400

    install: @codemirror/lang-javascript

--- components/ide.js
+++ components/ide.js
@@ -1,4 +1,17 @@
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
 export default function IDE() {
-    return(myView);
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log('value:', value);
+  }, []);
+  return (
+    <CodeMirror
+      value="console.log('hello world!');"
+      height="200px"
+      extensions={[javascript({ jsx: true })]}
+      onChange={onChange}
+    />
+  );
 }
