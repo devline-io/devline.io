d213c1c05df6b0ef281c7b0cd01e2c169b5a6695
GreenLightning07 <cwnaze@gmail.com>
2023-08-10 12:14:37 -0400

    style: fixed styles
    reverted styles back to normal
    properly styled the code editor

--- components/codeeditor.js
+++ components/codeeditor.js
@@ -2,8 +2,6 @@ import { Editor } from "@monaco-editor/react";
 
 export default function CodeEditor() {
     return (
-      <section>
-        <Editor height='100vh' language='javascript' theme='vs-dark' defaultValue="console.log('Hello World!')"/>
-      </section>
+      <Editor className="noGlobal" height='100vh' language='javascript' theme='vs-dark' defaultValue="console.log('Hello World!')"/>
     )
 }
