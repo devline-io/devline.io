c8ff27989d69461c63d59fc96bd21624035f454c
Dylan <dylan.kid@icloud.com>
2023-09-19 12:28:17 -0400

    fix: made profile not reachable unless signed in

--- app/dashboard/profile/profile.jsx
+++ app/dashboard/profile/profile.jsx
@@ -1,6 +1,30 @@
 'use client'
 
+import { getAuth } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { motion } from 'framer-motion';
+import { initFirebase } from '../../../components/firebase';
+import { useState, useEffect, useRef } from 'react';
+
+
 export default function Profile() {
+
+    initFirebase();
+    const auth = getAuth();
+    const router = useRouter();
+
+    const [user, loading] = useAuthState(auth);
+
+    useEffect(() => {
+        if(user) {
+            return;
+        }
+        if(!user && !loading) {
+            console.log(user);
+            router.push('/');
+        }
+    })
 
     return (
         <h1>Test</h1>
