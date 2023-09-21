923e24066ce42bfdfcbc200e1c1e084b217d5644
Dylan Keller <dekeller2024@loyolablakefield.org>
2023-09-21 09:04:44 -0400

    feat: back button to profile page

--- app/dashboard/profile/profile.jsx
+++ app/dashboard/profile/profile.jsx
@@ -1,11 +1,13 @@
 'use client'
 
+import Image from 'next/image';
 import { getAuth } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { motion } from 'framer-motion';
 import { initFirebase } from '../../../components/firebase';
 import { useState, useEffect, useRef } from 'react';
+import styles from '../../../styles/profile.module.css';
 
 
 export default function Profile() {
@@ -27,6 +29,14 @@ export default function Profile() {
     })
 
     return (
-        <h1>Test</h1>
+        <>
+            <div className={styles.upperWrapper}>
+                <div className={styles.upperContainer}>
+                    <button onClick={()=>router.push('/dashboard')} className={styles.backButton}>Back</button>
+                    <div className={styles.notificationBell}>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
