c7c42761b14f7e2ef72fb9c6d06a32d4a5812956
Casey Nazelrod <cwnaze@gmail.com>
2023-11-20 09:20:45 -0500

    fix: moved course page into dedicated course folder

--- app/courses/test-couse/new-course.jsx
+++ app/courses/test-couse/new-course.jsx
@@ -1,15 +1,15 @@
 'use client'
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import Navbar from "../../components/navbar";
+import Navbar from "../../../components/navbar";
 import { useRef, useState, useEffect } from "react";
-import { initFirebase } from "../../components/firebase";
+import { initFirebase } from "../../../components/firebase";
 import { getAuth } from "firebase/auth";
 import Link from 'next/link';
 import { useRouter } from "next/navigation";
 import styles from '../../styles/courses.module.css';
 import { PythonProvider } from 'react-py';
-import CodeEditor from '../../components/code-editor';
+import CodeEditor from '../../../components/code-editor';
 
 export default function NewCourse() {
     initFirebase();
@@ -46,28 +46,12 @@ export default function NewCourse() {
 
     return (
         <>
-        {/*
             {profilePic && <Navbar 
               navItems={navItems} 
               navbarRef={nav}
               button={<button onClick={()=>router.push('/')} className={styles.alternateButton}>Upgrade</button>} 
               profilePic={profilePic} 
             />}
-            */}
-
-            <main>
-                <div className={styles.container}>
-                    <div className={styles.content}>
-                        <h1>Course Title </h1>
-                        <h3>Course Sub Title </h3>
-                    </div>
-                    <div className={styles.codeEditor}>
-                    <PythonProvider>
-                            <CodeEditor/>
-                    </PythonProvider>
-                    </div>
-                </div>
-            </main>
         </>
     )
 }
