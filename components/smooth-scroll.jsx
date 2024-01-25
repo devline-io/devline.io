68882be59a6faca4d4253fd0627ad3298752afd6
Casey Nazelrod <cwnaze@gmail.com>
2024-01-25 13:25:15 -0500

    feat: get position of items in function

--- components/smooth-scroll.jsx
+++ components/smooth-scroll.jsx
@@ -1,3 +1,5 @@
-export default function SmoothScroll(position, navHeight) {
+export default function SmoothScroll(scrollItem, nav) {
+    const position = scrollItem.current.offsetTop;
+    const navHeight = nav.current.offsetHeight;
     window.scrollTo({behavior: 'smooth', top: (position - navHeight) * getComputedStyle(document.querySelector('html')).zoom});
 }
