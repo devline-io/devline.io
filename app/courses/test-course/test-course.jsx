8d89911b6c2d959cd36a4fdd1f6287672d4fdfad
Casey Nazelrod <cwnaze@gmail.com>
2024-02-02 14:18:25 -0500

    feat: can navigate units from outline

--- app/courses/test-course/test-course.jsx
+++ app/courses/test-course/test-course.jsx
@@ -103,7 +103,7 @@ export default function TestCourse({props}) {
                                     {props.units[chapter].map((unit, uIndex) => {
                                         return (
                                             <li>
-                                                {unit}
+                                                <a onClick={() => handleLessonClick(`c1u${uIndex+1}`, uIndex + 1)}>{unit}</a>
                                                 <ul>
                                                     {props.lessons[chapter][unit].map((lesson,lIndex) => {
                                                         return <li className={styles.outlineLesson}><a onClick={() => handleLessonClick(`c1u${uIndex+1}l${lIndex+1}`, uIndex + 1)}>{lesson}</a></li>
