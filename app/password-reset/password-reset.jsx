051c445f71a63c770599ee911e3b509a8b73527a
Casey Nazelrod <cwnaze@gmail.com>
2024-02-06 13:41:46 -0500

    style: fixed form style and added back buton

--- app/password-reset/password-reset.jsx
+++ app/password-reset/password-reset.jsx
@@ -4,10 +4,12 @@ import styles from '../../styles/form.module.css';
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { useRef } from 'react';
 import { initFirebase } from '../../components/firebase';
+import { useRouter } from 'next/navigation';
 
 export default function PasswordReset() {
     initFirebase();
     const auth = getAuth();
+    const router = useRouter();
 
     const email = useRef(null);
 
@@ -23,13 +25,16 @@ export default function PasswordReset() {
     return(
         <div className={styles.wrapper}>
             <div className={styles.container}>
+                <div className={styles.backButton}>
+                    <a onClick={() => router.push('../sign-in')}>&larr; Back</a>
+                </div>
                 <h1>Reset Password</h1>
-                <form>
+                <form className={styles.form}onSubmit={handleSubmit}>
                     <div>
                         <label className={styles.email} htmlFor='email'>Email</label>
                         <input ref={email} required id='email' type='text'/>
                     </div>
-                    <button className={styles.fullButton} onClick={handleSubmit}>Submit</button>
+                    <button className={styles.fullButton}>Submit</button>
                 </form>
             </div>
         </div>
